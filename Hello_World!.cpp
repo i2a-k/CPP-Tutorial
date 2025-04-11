eea50f684cd5bb64e71bef456f8661b75113012f
AmirAli <35394946+i2a-k@users.noreply.github.com>
2025-04-11 23:56:21 +0330

    Getting Started with C++

---
+++ Hello_World!.cpp
@@ -0,0 +1,17 @@
+//Introduction to C++
+
+/*
+First Project
+Hello World!
+*/
+
+#include <iostream> //Loading Libraries. <iostream> controls inputs and outputs (cout, cin)
+
+using namespace std; //Standard commands
+//every command in C++ ends with ;
+
+int main() //Main function of C++ source. Codes execute in main.
+{ //The block of codes are placed between {}
+    cout << "Hello World!"; //Print a string
+
+}
